6400aaa2ccd0dba731e777feac3e344eb724cdd2
Sacha Greif <sacha357@gmail.com>
2016-02-25 15:23:50 +0900

    version 0.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "utilities:smart-methods",
   summary: "Smart methods",
-  version: "0.0.1",
+  version: "0.0.2",
   git: "https://github.com/meteor-utilities/smart-methods.git"
 });
 
