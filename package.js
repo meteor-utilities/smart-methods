74220891b536e981ddb870a0c3252ea7e29bd37c
Sacha Greif <sacha357@gmail.com>
2016-06-16 10:29:13 +0900

    fix #1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "utilities:smart-methods",
   summary: "Smart methods",
-  version: "0.1.4",
+  version: "0.1.5",
   git: "https://github.com/meteor-utilities/smart-methods.git"
 });
 
